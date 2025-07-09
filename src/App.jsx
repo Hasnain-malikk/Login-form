090b54c73caf49c0abb794479a755b4c16dc2b06
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2025-07-09 14:47:12 +0500

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './App.css'
+import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+import Navbar from './Components/Navbar/Navbar'
+import Button from './Components/Button/Button'
 
 const App = () => {
   return (
     <div>
-      helo
+      <Header/>
+      <Main/>
+      <Navbar/>
+      <Button/>
     </div>
   )
 }
