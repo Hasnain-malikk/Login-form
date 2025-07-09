82c5341435d84dc642277df23ecfa32c34c6027c
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2025-07-09 17:20:52 +0500

    input fields added

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Main.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHand } from '@fortawesome/free-regular-svg-icons'
 
 const Main = () => {
   return (
     <div className='main'>
-      this is main
+      <div className="left">
+        <h4 className='form-heading'>Welcome 👋</h4>
+        <p className='p1'>Today is a new day. It's your day. You shape it. 
+             Sign in to start managing your projects.</p>
+        <div className="input">
+            <p className='p2'>Email</p>
+            <input type="text" placeholder='Example@gmail.com' />
+            <p id='pass' className='p2'>Password</p>
+            <input type="password" placeholder='At least 8 character'/>
+        </div>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
